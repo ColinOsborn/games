d0c5a854dd464ce87612faa1cd0720f6f8cb45c5
Colin Osborn <Colin.Osborn@gmail.com>
2017-11-07 20:54:05 -0700

    okay I need some practice

--- year.rb
+++ year.rb
@@ -1,48 +1,5 @@
 require 'pry'
 
   def century_from_year(year)
-    case year
-    when 0..100
-      return 1
-    when 101..200
-      return 2
-    when 201..300
-      return 3
-    when 301..400
-      return 4
-    when 401..500
-      return 5
-    when 501..600
-      return 6
-    when 601..700
-      return 7
-    when 701..800
-      return 8
-    when 801..900
-      return 9
-    when 901..1000
-      return 10
-    when 1001..1100
-      return 11
-    when 1101..1200
-      return 12
-    when 1201..1300
-      return 13
-    when 1301..1400
-      return 14
-    when 1401..1500
-      return 15
-    when 1501..1600
-      return 16
-    when 1601..1700
-      return 17
-    when 1701..1800
-      return 18
-    when 1801..1900
-      return 19
-    when 1901..2000
-      return 20
-    when 2001..2100
-      return 21
-    end
+    ((year - 1) / 100) + 1
   end
