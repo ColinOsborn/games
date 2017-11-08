8e3d9339608f01bebc5e1d4f98ccbe452d70e79d
Colin Osborn <Colin.Osborn@gmail.com>
2017-11-07 20:50:42 -0700

    all tests are passing

--- year_test.rb
+++ year_test.rb
@@ -54,4 +54,74 @@ class YearTest < Minitest::Test
     result = century_from_year(1001)
     assert_equal 11, result
   end
+
+  def test_10_century
+    result = century_from_year(987)
+    assert_equal 10, result
+  end
+
+  def test_niner_century
+    result = century_from_year(888)
+    assert_equal 9, result
+  end
+
+  def test_crazy_eights
+    result = century_from_year(717)
+    assert_equal 8, result
+  end
+
+  def test_satans_year
+    result = century_from_year(666)
+    assert_equal 7, result
+  end
+
+  def test_a_fiver
+    result = century_from_year(505)
+    assert_equal 6, result
+  end
+
+  def test_some_four_play
+    result = century_from_year(487)
+    assert_equal 5, result
+  end
+
+  def test_some_three
+    result = century_from_year(323)
+    assert_equal 4, result
+  end
+
+  def test_from_code_fights
+    result = century_from_year(374)
+    assert_equal 4, result
+  end
+
+  def test_terrible_two
+    result = century_from_year(222)
+    assert_equal 3, result
+  end
+
+  def test_from_code_fights_2
+    result = century_from_year(200)
+    assert_equal 2, result
+  end
+
+  def test_some_one
+    result = century_from_year(115)
+    assert_equal 2, result
+  end
+
+  def test_early_early_time
+    result = century_from_year(8)
+    assert_equal 1, result
+  end
+
+  def test_some_shitty_year
+    result = century_from_year(2010)
+    assert_equal 21, result
+  end
+
+  def test_one_edge
+    result = century_from_year(2000)
+    assert_equal 20, result
+  end
 end
